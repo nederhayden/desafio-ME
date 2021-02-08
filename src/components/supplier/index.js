cbe6704cca94017a64b70303b5d569407b84a7f2
Neder <nedersimoes@gmail.com>
2021-02-08 17:09:09 -0400

    implementando container supplier

--- src/components/supplier/index.js
+++ src/components/supplier/index.js
@@ -1,10 +1,33 @@
 import React from "react";
-import { DivSupplier, Div1, DivRow, DivCode } from '../../components/supplier/style'
+import {
+  DivSupplier,
+  DivContainer,
+  DivRow,
+  DivCode,
+  DivValue,
+  DivContainer2,
+  DivContainer3,
+  DivNameEmail,
+  DivPhoneFax,
+} from "../../components/supplier/style";
+
+import user from "../../assets/user.svg";
+import phone from "../../assets/phone.svg";
+import fax from "../../assets/fax.svg";
+import eye from "../../assets/eye.svg";
+import backarrow from "../../assets/backarrow.svg";
+
+import moment from "moment";
 
 export default function Supplier({ supplierInfo }) {
+  const date = supplierInfo.readAt;
+  const date2 = supplierInfo.lastReplyAt;
+  const formattedDate = moment(date).format("L hh:mm");
+  const formattedDate2 = moment(date2).format("L hh:mm");
+
   return (
     <DivSupplier>
-      <Div1>
+      <DivContainer>
         <h3>Supplier</h3>
         <DivRow>
           <p>{supplierInfo.name}</p>
@@ -12,21 +35,43 @@ export default function Supplier({ supplierInfo }) {
             <p>Code #{supplierInfo.code}</p>
           </DivCode>
         </DivRow>
-        <p>{supplierInfo.document.value}</p>
+        <DivValue>
+          <p>{supplierInfo.document.value}</p>
+        </DivValue>
         <p>{supplierInfo.address}</p>
-      </Div1>
+      </DivContainer>
 
-      <div id="Column2">
-        {supplierInfo.contact.name}
-        {supplierInfo.contact.email}
-        {supplierInfo.contact.phone}
-        {supplierInfo.contact.fax}
-      </div>
+      <DivContainer2>
+        <DivNameEmail>
+          <span>
+            <p>
+              <img src={user} alt="" /> {supplierInfo.contact.name} -{" "}
+            </p>
+            <p1>{supplierInfo.contact.email}</p1>
+          </span>
+        </DivNameEmail>
+        <DivPhoneFax>
+          <span>
+            <p>
+              <img src={phone} alt="" /> {supplierInfo.contact.phone} -
+            </p>
+            <p>
+              <img src={fax} alt="" /> {supplierInfo.contact.fax}
+            </p>
+          </span>
+        </DivPhoneFax>
+      </DivContainer2>
 
-      <div id="Column3">
-        <p>Read: {supplierInfo.readAt}</p>
-        <p>Last Reply: {supplierInfo.lastReplyAt}</p>
-      </div>
+      <DivContainer3>
+        <span>
+          <p>
+            <img src={eye} alt="" /> Read: {formattedDate}
+          </p>
+          <p1>
+            <img src={backarrow} alt="" /> Last Reply: {formattedDate2}
+          </p1>
+        </span>
+      </DivContainer3>
     </DivSupplier>
   );
 }
--- src/components/supplier/style.js
+++ src/components/supplier/style.js
@@ -1,34 +1,90 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const DivSupplier = styled.div`
-    margin: 24px 12px;
-    box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.2);
-    border-radius: 12px;
-    border: 1px solid steelblue;
-    padding: 8px;
-    display: grid;
-    grid-template-columns:  1fr 1fr 1fr;
+  margin: 24px 12px;
+  padding: 8px;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 12px;
+  border: 1px solid steelblue;
+  display: flex;
+  justify-content: space-between;
 `;
 
-export const Div1 = styled.div`
-    h3 {
-        color: rgba(128, 128, 128, 0.555);
-    font-size: 16px;
+export const DivContainer = styled.div`
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  h3 {
+    padding-bottom: 6px;
+    color: rgba(128, 128, 128, 0.555);
+    font-size: 18px;
     font-weight: 200;
-    }
+  }
 `;
 
 export const DivRow = styled.div`
-    flex-direction: row;
-    display: flex;
-    font-weight: 500;
-    color: rgb(0, 119, 255);
+  padding-bottom: 6px;
+  flex-direction: row;
+  display: flex;
+  font-weight: 500;
+  font-size: 20px;
+  color: rgb(0, 119, 255);
 `;
 
 export const DivCode = styled.div`
-    background-color: lightblue;
-    color: gray;
-    margin-left: 8px;
-    padding: 2px;
-    border-radius: 12px;
+  background-color: lightblue;
+  color: gray;
+  font-size: 16px;
+  font-weight: bold;
+  margin-left: 8px;
+  padding: 4px;
+  border-radius: 18px;
+`;
+
+export const DivValue = styled.div`
+  padding-bottom: 8px;
+`;
+
+export const DivContainer2 = styled.div`
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
+
+export const DivNameEmail = styled.div`
+  padding-bottom: 8px;
+  span {
+    display: flex;
+    flex-direction: row;
+  }
+  img {
+    width: 18px;
+  }
+  p1 {
+    color: #028dcd;
+  }
+`;
+
+export const DivPhoneFax = styled.div`
+  span {
+    display: flex;
+    flex-direction: row;
+  }
+  img {
+    width: 18px;
+  }
+`;
+
+export const DivContainer3 = styled.div`
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  img {
+    width: 18px;
+  }
+  p {
+    padding-bottom: 8px;
+  }
 `;
